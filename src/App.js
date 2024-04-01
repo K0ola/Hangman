af9a8357d51e9c18e8e7cd92516527a1050c9947
K0la <124051933+K0ola@users.noreply.github.com>
2024-04-01 19:36:39 +0200

    :diamonds:

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Scene from './Scene';
+import Game from './game/game';
+import './styles/app.css';
+
+function App() {
+  return (
+    <div>
+      <h1> </h1>
+      <Game />
+      <Scene />
+    </div>
+  );
+}
+
+
+
+export default App;
